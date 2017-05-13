d833675b663f36a78bbf0660004358183217e6a0
Minwoo Park <park400@indiana.edu>
2017-05-12 20:28:46 -0400

    Finalized

--- NanoDgreePractice/RecordSoundsViewController.swift
+++ NanoDgreePractice/RecordSoundsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecordSoundsViewController.swift
 //  NanoDgreePractice
 //
 //  Created by Minwoo Park on 5/10/17.
@@ -9,12 +9,14 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
- 
+    var audioRecorder: AVAudioRecorder!
+    
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,25 +26,68 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("ViewWillApper Called")
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    @IBAction func recordAudio(_ sender: Any) {
-        print("Record button clicked")
-        recordingLabel.text = "Recording in Progress"
-        stopButton.isEnabled = true
-        recordButton.isEnabled = false
-    }
-    @IBAction func stopAudio(_ sender: Any) {
-        print("Recording Stopped")
-        recordingLabel.text = "Tap to Record"
-        recordButton.isEnabled = true
-        stopButton.isEnabled = false
+        
     }
 
+    @IBAction func recordAudio(_ sender: Any) {
+        changeButtonAndLabel(recordingStatus: "Recording in Progress", enablingRecordButton: false, enablingStopButton: true)
+//        recordingLabel.text = "Recording in Progress"
+//        stopButton.isEnabled = true
+//        recordButton.isEnabled = false
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+       
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
+        
+    }
+    @IBAction func stopAudio(_ sender: Any) {
+        changeButtonAndLabel(recordingStatus: "Tap to Record", enablingRecordButton: false, enablingStopButton: true)
+//        recordingLabel.text = "Tap to Record"
+//        recordButton.isEnabled = true
+//        stopButton.isEnabled = false
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        
+        if flag{
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        
+        }else{ 
+            print("Record Fail")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+    
+    func changeButtonAndLabel(recordingStatus: String, enablingRecordButton: Bool, enablingStopButton: Bool){
+        
+        recordingLabel.text = recordingStatus
+        recordButton.isEnabled = enablingRecordButton
+        stopButton.isEnabled = enablingStopButton
+        
+    }
 }
 
  
